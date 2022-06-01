99ed87c6af1e2d3193507ef2f6b4ac4d6cd34c28
holethanhbao030695 <holethanhbao030695@gmail.com>
2022-06-01 14:32:59 +0700

    upload code

---
+++ main.js
@@ -0,0 +1,67 @@
+
+/*---Bài Tập 1---*/
+//Khởi tạo biến
+var luong = 0;
+var songay = 0;
+var tinhluong = 0;
+//Gán giá trị cho biến
+luong = 100000
+songay = 30
+tinhluong = luong * songay;
+console.log("Tiền lương của nhân viên là:", tinhluong);
+
+/*---Bài Tập 2---*/
+//Khởi tạo biến
+var so1 = 0;
+var so2 = 0;
+var so3 = 0;
+var so4 = 0;
+var so5 = 0;
+var trungbinh = 0;
+//Gán giá trị cho biến
+so1 = 4.5;
+so2 = 7.25;
+so3 = 9.75;
+so4 = 11.9;
+so5 = 15.1;
+trungbinh = ((so1 + so2 + so3 + so4 + so5) / 5);
+console.log("Giá trị trung bình số thực là:", trungbinh);
+
+/*---Bài Tập 3---*/
+//Khởi tạo biến
+var USD = 0;
+var nhap = 0;
+var tienquydoi = 0;
+//Gán giá trị cho biến
+USD = 23500;
+nhap = 2;
+tienquydoi = nhap * USD;
+console.log("Số tiền sau quy đổi là: ", tienquydoi + "VND");
+
+/*---Bài Tập 4---*/
+//Khởi tạo biến
+var CD = 0;
+var CR = 0;
+var DT = 0;
+var CV = 0;
+//Gán giá trị cho biến
+CD = 10;
+CR = 5;
+DT = CD * CR;
+CV = (CD + CR) * 2;
+console.log("Diện tích của HCN là:", DT);
+console.log("Chu vi của HCN là:", CV);
+
+/*---Bài Tập 5---*/
+var n = 78;
+var m = 99;
+var hangchuc = Math.floor(78 / 10)
+var hangdonvi = Math.floor(78 % 10);
+
+var hangchuc1 = Math.floor(99 / 10);
+var hangdonvi1 = Math.floor(99 % 10);
+
+var total = hangchuc + hangdonvi;
+console.log("Tổng 2 ký số:", total);
+var total1 = hangchuc1 + hangdonvi1;
+console.log("Tổng 2 ký số:", total1);
